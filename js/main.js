75e8b1f8258212ffbbd80f752f16fae53abd8f92
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-26 11:44:34 +0200

    yay!

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 const updateBtn = document.querySelector('.button');
 const selectMood = document.querySelector('.select');
 const face = document.querySelector('.face');
+const pag = document.querySelector('.pag__body');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -21,6 +22,13 @@ function updateMood() {
     /*Generará un número aleatorio de 100 como máximo y si es un número par colocará el color de fondo del amarillo correcto (#ffcc00) y si es impar, usaremos el naranja fuego chileno (#ff9900). */
     const randomNum = getRandomInt(100);
     console.log(randomNum);
+
+    if (randomNum % 2 === 0){//es par color amarillo #ffcc00 que es la de por defecto
+        pag.classList.remove('pag--alt');
+    } else { //es impar color naranja fuego chileno #ff9900
+        pag.classList.add('pag--alt');
+        
+    }
 
 }
 
